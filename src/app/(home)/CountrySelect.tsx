2f57243ed0c24e2a7d209d12bf7799bf1fdde372
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-08-22 02:59:54 -0300

    feat: create CountrySelect component

---
+++ src/app/(home)/CountrySelect.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+// DEPENDENCY
+import { useState } from 'react'
+import Flag from 'react-world-flags'
+
+// COMPONENT
+import {
+  SelectContent,
+  SelectItem,
+  SelectRoot,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/Form/Select'
+
+// UTIL
+import { countries } from '@/utils/countries'
+
+// TYPE
+type CountryCode = keyof typeof countries
+
+export function CountrySelect() {
+  const [value, setValue] = useState<CountryCode | undefined>(undefined)
+
+  const handleCountryChange = (country: string) => {
+    setValue(country as CountryCode)
+  }
+  return (
+    <SelectRoot
+      name="country"
+      value={value}
+      onValueChange={handleCountryChange}
+    >
+      <SelectTrigger>
+        <SelectValue placeholder="Select your country..." />
+      </SelectTrigger>
+
+      <SelectContent>
+        {Object.entries(countries).map(([code, title]) => (
+          <SelectItem key={code} value={code}>
+            <span className="flex items-center gap-2">
+              <Flag
+                className="h-5 w-5 rounded-full object-cover object-center"
+                code={code}
+              />
+              {title}
+            </span>
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </SelectRoot>
+  )
+}
